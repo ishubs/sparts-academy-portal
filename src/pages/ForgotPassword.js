22b77c6895f37a50d9b73416eebed91821693e4c
shubham <shubbhu7717@gmail.com>
2023-07-29 16:48:30 +0530

    base

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import ImageLight from '../assets/img/forgot-password-office.jpeg'
+import ImageDark from '../assets/img/forgot-password-office-dark.jpeg'
+import { Label, Input, Button } from '@windmill/react-ui'
+
+function ForgotPassword() {
+  return (
+    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+      <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+        <div className="flex flex-col overflow-y-auto md:flex-row">
+          <div className="h-32 md:h-auto md:w-1/2">
+            <img
+              aria-hidden="true"
+              className="object-cover w-full h-full dark:hidden"
+              src={"https://images.unsplash.com/photo-1628779238951-be2c9f2a59f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"}
+              alt="Office"
+            />
+            <img
+              aria-hidden="true"
+              className="hidden object-cover w-full h-full dark:block"
+              src={"https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2207&q=80"}
+              alt="Office"
+            />
+          </div>
+          <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <div className="w-full">
+              <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Forgot password
+              </h1>
+
+              <Label>
+                <span>Email</span>
+                <Input className="mt-1" placeholder="Jane Doe" />
+              </Label>
+
+              <Button tag={Link} to="/login" block className="mt-4">
+                Recover password
+              </Button>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ForgotPassword
